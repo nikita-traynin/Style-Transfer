4130387852b628028ce6bbae2b43ff010855e7d6
Nikita Traynin <nikitatraynin@gmail.com>
2021-08-06 17:55:45 -0500

    Added serverside file upload functionality

--- app.py
+++ app.py
@@ -1,8 +1,36 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'img'
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return render_template("home.html")
+    return render_template('home.html')
+
+@app.route('/file-upload', methods=['POST'])
+def upload_file():
+    if 'content' in request.files:
+        subfolder = 'content'
+        file = request.files['content']
+    elif 'style' in request.files:
+        subfolder = 'style'
+        file = request.files['style']
+    else:
+        print('\n\n\n', request.files)
+        print('File upload name neither content nor style, exiting')
+        return redirect('/')
+
+    # if no file uploaded, display error msg
+    if file.filename == '':
+        flash('No selected file')
+        return redirect('/')
+
+    # if there is a file, save it
+    if file:
+        filename = secure_filename(file.filename)
+        dest = os.path.join(app.config['UPLOAD_FOLDER'], subfolder, filename)
+        print(dest)
+        file.save(dest)
+        return redirect('/')
