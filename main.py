0c024219a98bd75485c45027dd851265f2478ee8
Nikita Traynin <nikitatraynin@gmail.com>
2022-04-20 22:46:01 -0500

    moved configs

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from werkzeug.serving import run_simple
-import web # this runs init in web
-import MLModel # this runs init in MLModel
+import web  # this runs init in web
+import MLModel  # this runs init in MLModel
 import sys
-
 
 # when you run this module, you must input a single command line argument
 # which is the name of the app you want to run.
@@ -17,7 +16,7 @@ else:
     if __name__ == '__main__':
         if appname == "web":
             run_simple('localhost', 5000, web.app,
-                   use_reloader=True, use_debugger=True, use_evalex=True)
+                       use_reloader=True, use_debugger=True, use_evalex=True)
         elif appname == "MLModel":
             run_simple('localhost', 5001, MLModel.app,
-                   use_reloader=True, use_debugger=True, use_evalex=True)
+                       use_reloader=True, use_debugger=True, use_evalex=True)
