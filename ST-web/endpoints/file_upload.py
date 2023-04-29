fe9acb3c54e06043f96a47ff24e3bb2fee592319
nikita-traynin <42842433+nikita-traynin@users.noreply.github.com>
2023-04-29 00:59:28 -0500

    code split-up and clean-up (#2)
    * split up endpoints and config
    
    * split up endpoints and config
    
    * removing unused files
    
    * cleanup; switching to remote dropzone script

---
+++ ST-web/endpoints/file_upload.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import request, redirect, flash
+from werkzeug.utils import secure_filename
+
+from app import app
+
+
+@app.route('/file-upload/<img_category>', methods=['POST'])
+def file_upload(img_category):
+    if img_category in request.files:
+        file = request.files[img_category]
+    else:
+        print('\n\n\n', request.files)
+        print('File missing from file upload request.')
+        flash('File missing from file upload request.')
+        return redirect('/')
+
+    # if there is a file, save it
+    filename = secure_filename(file.filename)
+    dest = os.path.join(app.config['UPLOAD_FOLDER'], img_category, filename)
+    print(dest)
+    file.save(dest)
+    return redirect('/')
